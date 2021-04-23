a6166ab9a5fb5a999cd91ef7dca14f43abf7eaae
yellowredfox <info@moon-forest-design.com>
2021-04-23 19:41:49 +0900

    UPDATE

---
+++ sample-code/2/main.js
@@ -0,0 +1,22 @@
+{ // ブロックスコープ(START)
+"use strict";
+
+
+// jQueryを使うよ
+$(function() {
+  // スクロールされないとき 「Please Scroll」を表示
+  setTimeout(function() { // 3秒待つ
+    if ($(window).scrollTop() < 10) { // 3秒経っても上端から10px以上スクロールしない場合
+      $("#pleaseScroll").fadeIn(); // 「Please Scroll」を表示しスクロールを促す
+    }
+  }, 3000);
+
+  // スクロールされたら「Please Scroll」を非表示
+  $(window).scroll(function() {
+    $("#pleaseScroll").fadeOut();
+  });
+
+});
+
+
+} // ブロックスコープ(END)

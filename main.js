4c66bc407819bf36a21f2589e07647d07eb57186
yellowredfox <info@moon-forest-design.com>
2021-05-04 19:57:21 +0900

    UPDATE

--- main.js
+++ main.js
@@ -16,21 +16,17 @@ $(function() {
     }, 500);
   });
 
-  // ページ底部の上端を境にトップへ戻るの表示非表示
-  const backToTop = () => {
-    const $docHeight = $(document).height(); // ページ全体の高さ
-    const $winHeight = $(window).height(); // 表示領域の高さ
-    const $bottomPage = $docHeight - $winHeight - 20; // ページ全体の高さから表示領域の高さを引いて底部ページの上端を算出　調整20
-    if ( $(this).scrollTop() > $bottomPage ) {
-      $("#backToTopBtn").fadeIn(); // ページ底部の上端までスクロールされたらトップへ戻るを表示
-    } else {
-      $("#backToTopBtn").hide(); // ページ底部の上端までスクロールが達していない場合はトップへ戻るを非表示
-    }
-  };
-
-  // スクロールされたときのイベントハンドラ
+  // トップへ戻るを表示・非表示する
   $(window).scroll(function() {
-    backToTop(); // ページ底部の上端を境にトップへ戻るの表示非表示
+    const $winHeight = $(this).height(); // ウィンドウ画面の高さを取得
+    const $scrollTop = $(this).scrollTop(); // スクロールした現在のtopの位置を取得
+    const $scrollBottom = $scrollTop + $winHeight; // スクロールした現在のtopの位置にウィンドウ画面の高さを加算してbottomの位置を算出
+    const $borderLine = $(document).height() * 0.7; // ページの高さの7割くらいを基準線にする 
+    if ( $scrollBottom > $borderLine ) {
+      $("#backToTopBtn").fadeIn(); // スクロールして基準線を超えたらトップへ戻るを表示
+    } else {
+      $("#backToTopBtn").hide(); // スクロールが基準線に達しない場合はトップへ戻るを非表示
+    }
   });
 
   // スムーズスクロール

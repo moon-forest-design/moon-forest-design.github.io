72526369e90231c15f2015c627ad44eb4a96911b
yellowredfox <info@moon-forest-design.com>
2021-05-07 07:09:24 +0900

    UPDATE

--- main.js
+++ main.js
@@ -4,6 +4,29 @@
 
 // jQueryを使うよ
 $(function() {
+  // justify-content: between;の最後の行を左寄せにする
+  const $aId = ["#sampleCode", "#sampleSite"]; // 左寄せにしたい親要素のIDを入力しておく
+  for (let $i = 0; $i < $aId.length; $i++) { // 
+    const $cellWidth = $(`${$aId[$i]} .card`).outerWidth(true); // 子要素の幅を取得
+    const $cellCount = $(`${$aId[$i]} .card`).length; // 子要素の数を取得
+    const $winWidth = $(window).width(); // ウィンドウ画面の幅を取得
+    const $columnCount = Math.trunc( $winWidth / $cellWidth ); // ウィンドウ画面に設定可能なカラム数を算出
+    console.log("$columnCount : " + $columnCount);
+    console.log("$cellCount : " + $cellCount);
+    let $lackCell = 0; // 左寄せするために不足している子要素の数を最終的に入れる
+    let $gridCount = 0; // 行列（グリッド）にした場合の子要素の総数を計算するときに使用
+    while ($gridCount < $cellCount) { // 行行列（グリッド）にした場合の子要素の総数を算出
+      $gridCount = $gridCount + $columnCount;
+      console.log("$gridCount : " + $gridCount);
+    }
+    $lackCell = $gridCount - $cellCount; // 左寄せするために不足している子要素の数を算出
+    console.log("$lackCell : " + $lackCell);
+    for (let $j = 0; $j < $lackCell; $j++) { // 左寄せするために不足している子要素の数だけ空要素を付け加える
+      $(`${$aId[$i]} .cards`).append('<div class="card isEmpty"></div>');
+      console.log("$j : " + $j);
+    }
+  }
+
 
   // サンプルの説明を表示する
   $('.cardThumbnail').hover(function() {
@@ -15,6 +38,7 @@ $(function() {
       top: "189px",
     }, 500);
   });
+
 
   // トップへ戻るを表示・非表示する
   $(window).scroll(function() {
@@ -28,6 +52,7 @@ $(function() {
       $("#backToTopBtn").hide(); // スクロールが基準線に達しない場合はトップへ戻るを非表示
     }
   });
+
 
   // スムーズスクロール
   $('a[href^="#"]').click(function() {

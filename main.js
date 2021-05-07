3a52630ba53d733ee0fa391c2654acb5c456b8ee
yellowredfox <info@moon-forest-design.com>
2021-05-08 08:47:38 +0900

    UPDATE

--- main.js
+++ main.js
@@ -11,19 +11,14 @@ $(function() {
     const $cellCount = $(`${$aId[$i]} .card`).length; // 子要素の数を取得
     const $winWidth = $(window).width(); // ウィンドウ画面の幅を取得
     const $columnCount = Math.trunc( $winWidth / $cellWidth ); // ウィンドウ画面に設定可能なカラム数を算出
-    console.log("$columnCount : " + $columnCount);
-    console.log("$cellCount : " + $cellCount);
     let $lackCell = 0; // 左寄せするために不足している子要素の数を最終的に入れる
     let $gridCount = 0; // 行列（グリッド）にした場合の子要素の総数を計算するときに使用
-    while ($gridCount < $cellCount) { // 行行列（グリッド）にした場合の子要素の総数を算出
-      $gridCount = $gridCount + $columnCount;
-      console.log("$gridCount : " + $gridCount);
+    while ($gridCount < $cellCount) { // 行列（グリッド）にした場合の子要素の総数を算出
+      $gridCount += $columnCount;
     }
     $lackCell = $gridCount - $cellCount; // 左寄せするために不足している子要素の数を算出
-    console.log("$lackCell : " + $lackCell);
     for (let $j = 0; $j < $lackCell; $j++) { // 左寄せするために不足している子要素の数だけ空要素を付け加える
       $(`${$aId[$i]} .cards`).append('<div class="card isEmpty"></div>');
-      console.log("$j : " + $j);
     }
   }
 

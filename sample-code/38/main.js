a77d8af5738d8481c2b09de42b703e42a5736dc6
yellowredfox <info@moon-forest-design.com>
2021-06-10 14:43:21 +0900

    UPDATE

--- sample-code/38/main.js
+++ sample-code/38/main.js
@@ -9,6 +9,7 @@ $(function() {
     autoplaySpeed: 3000, // 自動再生時の間隔ms
     speed: 1000, // 画像が切り替わる速度
     pauseOnHover: false, // 画像にマウスオーバしたときに停止するか？
+    dots: true, // ページネーションを表示するか？
   });
 });
 

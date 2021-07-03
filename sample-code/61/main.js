3651b48d047c72a75c2ed95af6d4f1620592e565
yellowredfox <info@moon-forest-design.com>
2021-07-03 17:37:19 +0900

    UPDATE

---
+++ sample-code/61/main.js
@@ -0,0 +1,29 @@
+{ // ブロックスコープ(START)
+"use strict";
+
+
+// jQueryを使うよ
+$(function() {
+  // ドロワーメニュー　drawer.js
+  $(document).ready(function() {
+    $('.drawer').drawer();
+  });
+
+  // スムーズスクロール
+  $(function() {
+    $('a[href^="#"]').click(function() { // a要素（href属性の値に#が付いているもの）をクリックしたら実行
+      const $href = $(this).attr("href"); // a要素のhref属性の中身を取得
+      const $adjust = $href === "#" ? 0 : 100 + 10; // 移動先がピッタリにならないよう調整する　トップ以外は上にヘッダと余白100 + 10px
+      const $target = $($href === "#" || $href === "" ? "html" : $href); // トップへ戻る場合は"html"を代入　他はaリンクのhrefの中身を代入
+      const $position = $target.offset().top - $adjust; // 移動先の要素のtop位置を取得　及び$adjustで調整
+      $('body, html').animate({
+        scrollTop: $position
+      }, 500, 'swing'); // animate()を使ってスムーズにページ内移動　スピード500、動作swing
+      return false;
+    });
+  });
+
+});
+
+
+} // ブロックスコープ(END)
